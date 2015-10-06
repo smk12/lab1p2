9838915f2e59d30c0488a3ced482295a50e9d460
smk12 <smk12@email.arizona.edu>
2015-10-05 18:59:52 -0700

    Update timer.c

--- timer.c
+++ timer.c
@@ -13,8 +13,8 @@ void delayUs(unsigned int delay)
 
     //TODO: Create a delay for "delay" micro seconds using timer 2
     TMR2 = 0;
-    PR2 = delay*6;
-    T2CONbits.TCKPS = 0;//pre-scalar
+    PR2 = delay*10-1;
+    T2CONbits.TCKPS = 3;//pre-scalar
     T2CONbits.TCS = 0;//osc config
     IFS0bits.T2IF = 0;//flag
     T2CONbits.ON = 1;
